84d7236f3e73028fbecebe70634623ca77a5aeb7
zlhunzzing <zlhunzzing@gmail.com>
2020-04-24 17:14:24 +0900

    메인 페이지 뼈대 완성

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -3,9 +3,55 @@ import { Link } from "react-router-dom";
 
 function Main() {
   return (
-    <div className="Main">
-      LeuniWorld!<br></br>
-      <Link to="/signup">아이디가 없으신가요? - 회원가입</Link>
+    <div
+      className="Main"
+      style={{
+        textAlign: "center",
+        width: "1024px",
+        // border: "1px solid black",
+      }}
+    >
+      <header>
+        <div
+          className="SignUp"
+          style={{
+            textAlign: "right",
+          }}
+        >
+          <Link to="/signup">로그인</Link>
+        </div>
+        <Link
+          className="Title"
+          to="main"
+          style={{
+            color: "black",
+            fontSize: "30px",
+          }}
+        >
+          르니월드
+        </Link>
+      </header>
+      <div
+        className="Menu"
+        style={{
+          margin: "20px",
+        }}
+      >
+        <span
+          style={{
+            padding: "5px",
+          }}
+        >
+          무설치게임
+        </span>
+        <span
+          style={{
+            padding: "5px",
+          }}
+        >
+          무지개의 기적(준비중)
+        </span>
+      </div>
     </div>
   );
 }
