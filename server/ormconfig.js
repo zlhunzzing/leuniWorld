21cf953c46bf6aa6c11dd398432f14104a904b88
zlhunzzing <zlhunzzing@gmail.com>
2020-05-14 22:49:49 +0900

    개발 환경 분리
    - npm run dev : NODE_ENV=development와 그에 맞는 DB 사용

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,13 +1,21 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+const dbName = (() => {
+  if (process.env.NODE_ENV) {
+    return `${process.env.TYPEORM_DATABASE}_${process.env.NODE_ENV}`;
+  } else {
+    return process.env.TYPEORM_DATABASE;
+  }
+})();
+
 module.exports = {
   type: process.env.TYPEORM_TYPE,
   host: process.env.TYPEORM_HOST,
   port: process.env.TYPEORM_PORT,
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
+  database: dbName,
   entities: ["src/entity/**/*.ts"],
   logging: false,
   synchronize: true,
